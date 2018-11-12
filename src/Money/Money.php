6ade2c29b1fe1911d3a6ac792d5157f9e914113e
kyamashiro <yamashiro@rrr-z.co.jp>
2018-11-13 08:01:25 +0900

    Factory Method Pattern

--- src/Money/Money.php
+++ src/Money/Money.php
@@ -9,12 +9,24 @@
 namespace Money;
 
 
-class Money
+abstract class Money
 {
     protected $amount;
+
+    abstract function times(int $multiplier): Money;
 
     public function equals(Object $object): bool
     {
         return ($object instanceof Money) && $this->amount === $object->amount && get_class($object) === get_class($this);
+    }
+
+    public static function dollar(int $amount): Money
+    {
+        return new Dollar($amount);
+    }
+
+    public static function franc(int $amount): Money
+    {
+        return new Franc($amount);
     }
 }
